7d9685a2769a985333d9a26f1be70f720f978ada
peterjbachman <peter.j.bachman@gmail.com>
2023-04-27 13:28:08 -0500

    Split data into training and test data

--- 03_split_cases.R
+++ 03_split_cases.R
@@ -1,11 +1,23 @@
 df <- read.csv("data/uncleaned/cases_plain.csv")
-df_filtered <- subset(df, !is.na(df$opinion_author))
-nrow(df_filtered)
-df$opinion_author
 
+# Remove cases without a female plaintiff similar to Glynn and Sen 2014\
+# Remove cases without a specific author as well.
+df <- subset(df, femplaintiff == 1 & per_curiam == "False")
 df$is_gender_issue <- ifelse(
   df$area %in% c("employment", "Title IX", "pregnancy", "abortion", "reproductive rights"),
   1,
   0
 )
-sum(df$is_gender_issue == 0)
+
+# Set up training and test dataset
+# Evenly split data by gender and non-gender issues
+set.seed(12345)
+df$training[df$is_gender_issue == 1] <- sample(c(0, 1), replace = TRUE)
+df$training[df$is_gender_issue == 0] <- sample(c(0, 1), replace = TRUE)
+
+test <- df[df$training == 0, ]
+train <- df[df$training == 1, ]
+
+# Write the training and test data
+write.csv(test, "data/cleaned/test_case.csv")
+write.csv(train, "data/cleaned/train_case.csv")
